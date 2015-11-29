61b07be591ebb69f41769a80ab497660376ec91b
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-29 09:22:10 -0500

    define behavior of employee

--- example01.rb
+++ example01.rb
@@ -11,7 +11,25 @@ class Employment
   keyword_initialize :employee, :boss
 
   role :boss
-  role :employee
+
+  role :employee do
+    def work_weekend
+      if fed_up?
+        quit
+      else
+        schedule_weekend_work
+      end
+    end
+
+    def quit
+      say("I'm sick of this place, #{boss.name}!")
+      say("I quit!")
+    end
+
+    def schedule_weekend_work
+      say("Saturday from 9am to 17pm...")
+    end
+  end
 end
 
 user1 = User.find(1)
